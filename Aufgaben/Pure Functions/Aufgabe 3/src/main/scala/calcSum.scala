bf03312bfaf1dba2bb98cac2d46a90b25cc89e89
stefanlaux <stefan.laux@edu.tbz.ch>
2024-03-19 10:10:03 +0100

    push

---
+++ Aufgaben/Pure Functions/Aufgabe 3/src/main/scala/calcSum.scala
@@ -0,0 +1,26 @@
+object calcSum {
+  // Not Recursive
+  def calculateSum(list: List[Int]): Int = {
+    list.sum
+  }
+
+  val numbers = List(1, 2, 3, 4, 5)
+  println(calculateSum(numbers))
+
+  // Recursive
+  def calculateSum(list: List[Int]): Int = {
+    @scala.annotation.tailrec
+    def calculateSumHelper(list: List[Int], acc: Int): Int = {
+      list match {
+        case Nil => acc
+        case head :: tail => calculateSumHelper(tail, acc + head)
+      }
+    }
+    calculateSumHelper(list, 0)
+  }
+
+  val numbers = List(1, 2, 3, 4, 5)
+  println(calculateSum(numbers))
+
+
+}
